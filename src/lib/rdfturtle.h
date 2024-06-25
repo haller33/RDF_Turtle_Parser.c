3f172ef774e879e29ffec38bf4570e9d4b0a2322
haller33 <eliseu42lucena@gmail.com>
2024-06-24 22:59:05 -0300

    some inicial follow up after reading the documentation of RDF 1.1

--- src/lib/rdfturtle.h
+++ src/lib/rdfturtle.h
@@ -47,9 +47,14 @@
 #define RDFTURTLE_JUST_MALLOC false
 #endif // RDFTURTLE_JUST_MALLOC
 
+#ifndef RDFTURTLE_COMMENT_CARACTER
+#define RDFTURTLE_COMMENT_CARACTER '#'
+#endif
+
 #ifndef RDFTURTLE_SEPARATION_CARACTER
-#define RDFTURTLE_SEPARATION_CARACTER '.'
-#define RDFTURTLE_CONTINUATION_CARACTER ';'
+#define RDFTURTLE_TRIPLE_SEPARATION_CARACTER '.'
+#define RDFTURTLE_CONTINUATION_PREDICATE_LIST_CARACTER ','
+#define RDFTURTLE_CONTINUATION_OBJECT_LIST_CARACTER ';'
 #define RDFTURTLE_SPACE_CARACTER " "
 #endif // RDFTURTLE_SEPARATION_CARACTER
 
@@ -62,20 +67,42 @@
 /// they can be up to 8000 caracters
 /// each cell can be a URI, so
 /// source :: https://stackoverflow.com/questions/417142/what-is-the-maximum-length-of-a-url-in-different-browsers#answer-417184
-#define RDFTURTLE_MAX_NAMESPACES 1500 // arbitrary one...
 #define RDFTURTLE_MAX_NAME_CELL 8192 // 2 ^ 13
 #define RDFTURTLE_MAX_CHAR_BY_LINE 25088 // ((8192 × 3) + 512)
+/// since there is no more wish to handle and keep data of the namespaces on the internal adt of the parser
+/// #define RDFTURTLE_MAX_NAMESPACES 1500 // arbitrary one...
 #endif // RDFTURTLE_MAX_NAMES
 
 #ifndef RDFTURTLE_SYMBOLS
 #define RDFTURTLE_SYMBOLS
-#define RDFTURTLE_BASE_URI_SYMBOL "@base"
-#define RDFTURTLE_PREFIX_SYMBOL "@prefix"
+#define RDFTURTLE_BEGIN_SYMBOL "@"
+#define RDFTURTLE_BASE_URI_SYMBOL_RDF11 "@base"
+#define RDFTURTLE_PREFIX_SYMBOL_RDF11 "@prefix"
+#define RDFTURTLE_BASE_URI_SYMBOL_SPARQL "BASE"
+#define RDFTURTLE_PREFIX_SYMBOL_SPARQL "PREFIX"
 #endif // RDFTURTLE_SYMBOLS
 
-// try to align the stack, since DoD and TenPowerRule
+/// return structure
+//
+typedef struct _rdf_turtle_adt_ret {
+
+    char rdf_base_url[RDFTURTLE_MAX_NAME_CELL];
+    char rdf_current_namespace[RDFTURTLE_MAX_NAME_CELL];
+    char rdf_current_prefix[RDFTURTLE_MAX_NAME_CELL];
+    char rdf_subject[RDFTURTLE_MAX_NAME_CELL];
+    char rdf_predicate[RDFTURTLE_MAX_NAME_CELL];
+    char rdf_object[RDFTURTLE_MAX_NAME_CELL];
+
+} rdf_turtle_adt_ret;
+
+/// try to align the stack, since DoD and TenPowerRule
 //
 typedef struct _rdf_turtle_adt {
+
+    size_t _triple_count;
+    size_t _caracter_count;
+    size_t _current_name_space_count;
+    size_t _current_line_count_iterator;
 
     char subject[RDFTURTLE_MAX_NAME_CELL];
     char predicate[RDFTURTLE_MAX_NAME_CELL];
@@ -83,16 +110,18 @@ typedef struct _rdf_turtle_adt {
 
     char _rdf_file_path[RDFTURTLE_MAX_NAME_CELL];
     char _rdf_base_uri[RDFTURTLE_MAX_NAME_CELL];
-    char _namespaces[RDFTURTLE_MAX_NAMESPACES][RDFTURTLE_MAX_NAME_CELL]; // TODO: remove it.
+    char _current_namespace[RDFTURTLE_MAX_NAME_CELL];
     char _current_line[RDFTURTLE_MAX_CHAR_BY_LINE];
-
-    size_t _triple_count;
-    size_t _column; // TODO: is needed ?
-    size_t _current_name_space_count;
-    size_t _current_line_count_iterator;
 
     FILE *_pdp;
 
 } rdf_turtle_adt;
 
 #endif // RDFTURTLE_PARSERC_H_
+
+#ifndef RDFTURTLE_PARSERC_IMPLEMENTATION
+#define RDFTURTLE_PARSERC_IMPLEMENTATION
+
+
+
+#endif // RDFTURTLE_PARSERC_IMPLEMENTATION
